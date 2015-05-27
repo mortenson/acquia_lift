148eb7a7829b8a81c49452e2ced001a05f0dcf62
Tim Plunkett <git@plnktt.com>
2015-05-27 16:23:58 -0400

    Only return the entity rendered, nothing else on page.

--- src/Controller/LiftController.php
+++ src/Controller/LiftController.php
@@ -10,6 +10,7 @@ namespace Drupal\lift\Controller;
 use Drupal\Core\Entity\Controller\EntityViewController;
 use Drupal\Core\Entity\EntityInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Returns responses for Lift routes.
@@ -18,15 +19,18 @@ class LiftController extends EntityViewController {
 
   /**
    * {@inheritdoc}
-   *
-   * @todo Remove once https://www.drupal.org/node/2495947 is in.
    */
   public function view(EntityInterface $entity, $view_mode = 'full', $langcode = NULL) {
-    $return = [];
+    $return = '';
+    // @todo Remove this check once https://www.drupal.org/node/2495947 is in.
     if ($this->entityManager->hasHandler($entity->getEntityTypeId(), 'view_builder')) {
-      $return = parent::view($entity, $view_mode, $langcode);
+      $render_array = parent::view($entity, $view_mode, $langcode);
+      // After generating the render array for the entity, render it to be the
+      // root of the page.
+      $return = $this->renderer->renderRoot($render_array);
     }
-    return $return;
+    // Directly set it as a response to bypass the rest of Drupal's theming.
+    return new Response($return);
   }
 
   /**
