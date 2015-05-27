9b31d5963ee111dcaa9f2bb3654f650fbf6a91ef
Tim Plunkett <git@plnktt.com>
2015-05-27 18:29:41 -0400

    Remove @todo about now-closed core issue.

--- src/Controller/LiftController.php
+++ src/Controller/LiftController.php
@@ -21,16 +21,12 @@ class LiftController extends EntityViewController {
    * {@inheritdoc}
    */
   public function view(EntityInterface $entity, $view_mode = 'full', $langcode = NULL) {
-    $return = '';
-    // @todo Remove this check once https://www.drupal.org/node/2495947 is in.
-    if ($this->entityManager->hasHandler($entity->getEntityTypeId(), 'view_builder')) {
-      $render_array = parent::view($entity, $view_mode, $langcode);
-      // After generating the render array for the entity, render it to be the
-      // root of the page.
-      $return = $this->renderer->renderRoot($render_array);
-    }
-    // Directly set it as a response to bypass the rest of Drupal's theming.
-    return new Response($return);
+    $render_array = parent::view($entity, $view_mode, $langcode);
+
+    // After generating the render array for the entity, render it to be the
+    // root of the page and return it as a response to bypass the rest of
+    // Drupal's theming.
+    return new Response($this->renderer->renderRoot($render_array));
   }
 
   /**
